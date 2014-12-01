98123a3c06d86b4fe4ff2509a48f6e965c6570ba
antonioortegajr <antonioortega@8808.local>
2014-11-30 19:37:53 -0800

    Added Ruby call. Fixed Typos
    Added Ruby call. Fixed Typos

--- get.php
+++ get.php
@@ -1,14 +1,13 @@
 <?php
 
-//begingin of endpoint. change if needed
+//beginning of endpoint. change if needed
 $gate = 'http://gateway.marvel.com/v1/public/';
 
-//Parameters change as you see fit. I picked charachter and Spidey
+//Parameters change as you see fit. I picked characters and Spidey
 $param = "characters?name=Spider-Man";
 
-
 //replace with your key from developers.marvel
-$APIKey ='YOUTKEY123';
+$APIKey ='YOURKEY123';
 
 //replace with your private key from developers.marvel
 $PrivateKey = 'YOURPRIVATEKEY123';
@@ -19,7 +18,7 @@ $timestamp = '1';
 //create hash
 $hash = md5($timestamp . $PrivateKey . $APIKey);
 
-
+//build endpoint
 $ch = curl_init($gate . $param . "&ts=" . $timestamp . "&apikey=" . $APIKey . "&hash=" . $hash);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HEADER, 0);
@@ -31,19 +30,6 @@ $data = curl_exec($ch);
 $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
 
 curl_close($ch);
-	
-// echo http code so I know hwat happened if it didn't work	
-echo $httpcode . '<br>';
-// echo info about the call so I can see if I need to change anything here
-echo $gate . $param . "&ts=" . $timestamp . "&apikey=" . $APIKey . $hash;
-
-echo '<br>MD5 hash is: ' . $hash;
-
-echo '<br>';
-
-echo 'Time Stamp: ' . $timestamp;
-
-echo '<br><br>';
 
 //dump the reposnse. should look like the docs at developers.marvel
     var_dump(
---
+++ get.rb
@@ -0,0 +1,52 @@
+require 'net/http'
+require 'digest/md5'
+
+#parts of the urls string
+api_amp = '&apikey='
+hash_amp = '&hash='
+ts_amp = '&ts='
+
+#replace with your key from developers.marvel
+marvel_api_key = 'YOURKEY123'
+
+#replace with your private key from developers.marvel
+marvel_private_key = 'YOURPRIVATEKEY123'
+
+#Timestamp, just used an integer for this example
+ts = '1'
+
+#Parameters change as you see fit. I picked characters and Spidey
+param = 'characters?name=Spider-Man'
+
+#beginning of endpoint. change if needed
+endpoint = 'http://gateway.marvel.com/v1/public/'
+
+#start building the url string
+url = endpoint.concat(param)
+url = url.concat(ts_amp)
+url = url.concat(ts)
+url = url.concat(api_amp)
+url = url.concat(marvel_api_key)
+
+#create hash
+hash = ts.concat(marvel_private_key)
+hash = hash.concat(marvel_api_key)
+
+digest = Digest::MD5.hexdigest(hash)
+puts digest
+
+
+
+#Build the rest of the url string for this call
+url = url.concat(hash_amp)
+url = url.concat(digest)
+
+
+#prefom a GET
+resp = Net::HTTP.get_response(URI.parse(url))
+
+resp_text = resp.body
+
+#the reposnse. should look like the docs at developers.marvel
+
+print resp_text
