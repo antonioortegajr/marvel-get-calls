557531c0e2a5e59e9348b836bc538f31f5660d1b
antonioortegajr <antonioortega@8808.local>
2014-12-07 14:28:58 -0800

    Changed concatenation
    Switched the concats to plus signs instead of .concat()

--- get.rb
+++ get.rb
@@ -22,23 +22,15 @@ param = 'characters?name=Spider-Man'
 endpoint = 'http://gateway.marvel.com/v1/public/'
 
 #start building the url string
-url = endpoint.concat(param)
-url = url.concat(ts_amp)
-url = url.concat(ts)
-url = url.concat(api_amp)
-url = url.concat(marvel_api_key)
+url = endpoint + param + ts_amp + ts + api_amp + marvel_api_key + hash_amp
 
-#create hash
-hash = ts.concat(marvel_private_key)
-hash = hash.concat(marvel_api_key)
+#create MD5 hash
+hash = ts + marvel_private_key + marvel_api_key
 
 digest = Digest::MD5.hexdigest(hash)
 
-
 #Build the rest of the url string for this call
-url = url.concat(hash_amp)
-url = url.concat(digest)
-
+url = url + digest
 
 #prefom a GET
 resp = Net::HTTP.get_response(URI.parse(url))
@@ -46,5 +38,4 @@ resp = Net::HTTP.get_response(URI.parse(url))
 resp_text = resp.body
 
 #the reposnse. should look like the docs at developers.marvel
-
 puts resp_text
